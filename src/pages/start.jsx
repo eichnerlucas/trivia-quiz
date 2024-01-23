4b3391fbc6f429907e99a8200494f04aa4a719ca
WypalaczRun <lucaseichner@yahoo.com.br>
2024-01-23 17:02:07 -0300

    Refactor preventDefault action in handleClick event

--- src/pages/start.jsx
+++ src/pages/start.jsx
@@ -12,7 +12,7 @@ export default function Start() {
 
     const handleClick = (evt) => {
         if (!isValid) {
-            evt.preventDefault();
+            return evt.preventDefault();
         }
 
         setIsButtonClicked(true)
