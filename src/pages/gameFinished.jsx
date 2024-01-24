e1b5d8b376cf740cf4b7695660ab02f495a772c2
WypalaczRun <lucaseichner@yahoo.com.br>
2024-01-24 02:34:31 -0300

    Update CSS in gameFinished modal window

--- src/pages/gameFinished.jsx
+++ src/pages/gameFinished.jsx
@@ -2,7 +2,7 @@ export default function GameFinished({ username, score }) {
     const scoreMessage = score.correct > score.incorrect ? '😊 Congratulations!' : '😞 Better luck next time,';
 
     return (
-        <div className="flex flex-col items-center bg-gray-800 min-w-96 max-w-96 overflow-auto p-6 mt-4 rounded-md shadow-sm">
+        <div className="flex flex-col items-center bg-gray-800 min-w-96 overflow-auto p-6 mt-4 rounded-md shadow-sm">
             <h1 className="p-1 text-xl text-white font-bold">{scoreMessage} {username}!</h1>
             <h2 className="p-1 text-white font-bold ">Your score is {score.correct} out of {score.correct + score.incorrect}</h2>
             <button
